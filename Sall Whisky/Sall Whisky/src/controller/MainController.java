fd510e20cf6476b16aee1f07f3a1c6cbaf49a2c2
MartinVilladsen <eaa23mkv@students.eaaa.dk>
2023-11-28 09:54:20 +0100

    CreateCask og observer

--- Sall Whisky/Sall Whisky/src/controller/MainController.java
+++ Sall Whisky/Sall Whisky/src/controller/MainController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainController {
+    List<Observer> observers = new ArrayList<>();
+
+    /**
+     * Opret og returner en Cask
+     * Pre: countryOfOrigin må ikke være null
+     * Pre: sizeInLiters > 0
+     * Pre: CaskSupplier og Position må ikke være null
+     */
+    public static Cask createCask(String countryOfOrigin, double sizeInLiters, String previousContent,
+                                  Position position, CaskSupplier supplier) {
+
+
+
+        return  null;
+    }
+
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+
 }
