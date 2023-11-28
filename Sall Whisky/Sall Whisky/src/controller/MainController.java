81621d67fa66083ec256937196b3301bbdd415a7
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2023-11-28 09:43:58 +0100

    method specifications for Controller part 2 - Iteration 1

--- Sall Whisky/Sall Whisky/src/controller/MainController.java
+++ Sall Whisky/Sall Whisky/src/controller/MainController.java
@@ -1,4 +1,38 @@
 package controller;
 
-public class MainController {
+import java.util.ArrayList;
+
+public abstract class MainController {
+
+
+    /**
+     * Finds warehouses that has atleast 1 rack which is not full
+     * @return Warehouses where isFilled = false
+     */
+    public static List<Warehouse> getAvailableWarehouses() {
+
+    }
+
+    /**
+     * Finds racks in a specific warehouse which are not full
+     * @param warehouse from which we want non-full racks
+     * @return Racks that are not full (isFilled = false)
+     */
+    public static List<Rack> getAvailableRacks(Warehouse warehouse) {
+
+    }
+
+    /**
+     * Finds shelves at a specific rack which are not full
+     * @param rack from which we want to find non-full shelves
+     * @return Shelves that are not full (isFilled = false)
+     */
+    public static List<Shelf> getAvailableShelves(Rack rack) {
+
+    }
+
+    /**
+     * 
+     */
+
 }
